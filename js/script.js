763cea0839d77ef43993b63b2676f0b9fa9d4e41
MarceloCardoso <marceloguimaraes780@gmail.com>
2023-07-01 18:48:44 -0300

    calculadora

---
+++ js/script.js
@@ -0,0 +1,15 @@
+alert('Bem vindo(a) à minha primeira calculadora! ❤');
+let Operação
+Operação = 'somar'
+
+let num1= prompt('Digite um número');
+let num2= prompt('Digite um número para ' + Operação);
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const Resultado = num1 + num2;
+
+const Resposta = alert(`O resultado da soma é  ${Resultado}`);
+
+alert('Obrigado 🐈')
